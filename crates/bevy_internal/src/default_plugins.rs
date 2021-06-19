fb555ddd1d9db8c6f19420eca143639687b4ed6a
Nguyễn Đức Long <duclong120995@gmail.com>
2021-06-19 09:57:35 +0700

    Implement mesh skinning

--- crates/bevy_internal/src/default_plugins.rs
+++ crates/bevy_internal/src/default_plugins.rs
@@ -40,6 +40,12 @@ impl PluginGroup for DefaultPlugins {
         #[cfg(feature = "bevy_winit")]
         group.add(bevy_winit::WinitPlugin::default());
 
+        #[cfg(feature = "bevy_animation_rig")]
+        group.add(bevy_animation_rig::AnimationRigPlugin::default());
+
+        #[cfg(feature = "bevy_render")]
+        group.add(RenderPlugin::default());
+
         #[cfg(feature = "bevy_render")]
         group.add(bevy_render::RenderPlugin::default());
 
