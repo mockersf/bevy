e3fde12f045f37cc1b488d304307f0d851c0188d
François <mockersf@gmail.com>
2023-03-24 23:22:52 +0100

    not supported on iOS

--- .github/start-mobile-example/specs/screenshot.js
+++ .github/start-mobile-example/specs/screenshot.js
@@ -5,9 +5,6 @@ const percyScreenshot = require('@percy/appium-app');
 describe('Running Bevy Example', () => {
   it('can take a screenshot', async () => {
 
-    const current_package = await browser.getCurrentPackage();
-    assert.equal(current_package, 'org.bevyengine.example');
-
     await browser.saveScreenshot('./screenshot.png');
 
     await percyScreenshot('Main Screen');
