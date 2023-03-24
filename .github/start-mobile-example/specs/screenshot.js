09dff94084632fd8da5f80885d6b3d316f9deaee
François <mockersf@gmail.com>
2023-03-24 23:45:02 +0100

    wait before taking screenshot

--- .github/start-mobile-example/specs/screenshot.js
+++ .github/start-mobile-example/specs/screenshot.js
@@ -5,6 +5,8 @@ const percyScreenshot = require('@percy/appium-app');
 describe('Running Bevy Example', () => {
   it('can take a screenshot', async () => {
 
+    await new Promise(r => setTimeout(r, 2000));
+
     await browser.saveScreenshot('./screenshot.png');
 
     await percyScreenshot('Main Screen');
