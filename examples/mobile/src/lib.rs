2e8817916a89ac5ca645a0a3a994a16f0bd02cf6
François <mockersf@gmail.com>
2023-03-25 23:04:52 +0100

    jkhkjhk

--- examples/mobile/src/lib.rs
+++ examples/mobile/src/lib.rs
@@ -83,6 +83,7 @@ fn setup_scene(
         point_light: PointLight {
             intensity: 5000.0,
             // Shadows makes some Android devices segfault, this is under investigation
+            // https://github.com/bevyengine/bevy/issues/8214
             #[cfg(not(target_os = "android"))]
             shadows_enabled: true,
             ..default()
