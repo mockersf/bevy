166bccd804e28d17f9a742e8c4eaa5806bdad7fe
François <mockersf@gmail.com>
2023-03-25 09:53:39 +0100

    rename screenshot per device

--- .github/start-mobile-example/specs/screenshot.js
+++ .github/start-mobile-example/specs/screenshot.js
@@ -12,7 +12,7 @@ describe('Running Bevy Example', () => {
     await browser.saveScreenshot('./screenshot.png');
 
     // Take screenshot for visual testing
-    await percyScreenshot('Main Screen');
+    await percyScreenshot(`Bevy Mobile Example - ${process.env.DEVICE} (OS ${process.env.OS_VERSION})`);
 
   });
 });
