8ea99b6f688a22e7792adb1bd831cdaae71759f2
François <mockersf@gmail.com>
2023-08-18 13:08:16 +0200

    ignore options

--- crates/bevy_render/src/renderer/mod.rs
+++ crates/bevy_render/src/renderer/mod.rs
@@ -129,7 +129,8 @@ pub async fn initialize_renderer(
         info!("found adapter: {:?}", adapter.get_info());
     }
     let adapter = instance
-        .request_adapter(request_adapter_options)
+        // .request_adapter(request_adapter_options)
+        .request_adapter(&RequestAdapterOptions::default())
         .await
         .expect(GPU_NOT_FOUND_ERROR_MESSAGE);
 
